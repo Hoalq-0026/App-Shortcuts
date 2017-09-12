7f637692e3a8f69ce39b799baf6be18aed72f43f
oLeQuangHoa <le.quang.hoa@framgia.com>
2017-09-12 17:29:13 +0700

    Initial project

---
+++ app/src/main/java/com/thaibaodigital/app_shortcuts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.thaibaodigital.app_shortcuts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
